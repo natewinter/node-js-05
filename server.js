de4f718c53a9bc243fb3c5b221ade63beb83bb73
Josh Brown <joshbrown2794@gmail.com>
2023-04-17 01:03:06 -0700

    server.js not serving pages

--- server.js
+++ server.js
@@ -11,11 +11,77 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf8');
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(data);
+  } catch (err) {
+    console.log(err);
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  const extenstion = path.extname(req.url);
+  const extension = path.extname(req.url);
   let contentType;
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+
+  // makes .html extension not required in the broweser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+  if (fileExists) {
+    //serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, { location: '/' });
+        res.end();
+        break;
+      default:
+        // serve a 404 response
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
 });
 //LISTEN FOR REQUESTS ALWAYS AT THE END OF SERVER.JS FILE
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
