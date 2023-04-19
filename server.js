30a4b86edabbb539c8f7704d4048f52c5ae32b75
Josh Brown <joshbrown2794@gmail.com>
2023-04-18 23:50:20 -0700

    data.json function working

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ const PORT = process.env.PORT || 3500;
 
 const serveFile = async (filePath, contentType, response) => {
   try {
-    const data = await fsPromises.readFile(filePath, 'utf8');
+    const rawData = await fsPromises.readFile(filePath, 'utf8');
+    const data =
+      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
     response.writeHead(200, { 'Content-Type': contentType });
-    response.end(data);
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
   } catch (err) {
     console.log(err);
     response.statusCode = 500;
